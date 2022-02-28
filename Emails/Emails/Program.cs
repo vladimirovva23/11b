0605adc2020041136b0a171b206f04484f0b5367
vladimirovva23 <petya.vladimirovva@abv.bg>
2022-02-28 10:40:01 +0200

    Emails

---
+++ Emails/Emails/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionaryclass = new Dictionary<string, string>();
+
+            while(true)
+            {
+                var input = Console.ReadLine().Split();
+                
+                if(input[0] == "Stop")
+                {
+                    break;
+                }
+
+                if(input[0] == "Add")
+                {
+                    string name = input[1];
+                    string email = input[2];
+
+                    if(!dictionaryclass.ContainsKey(name))
+                    {
+                        dictionaryclass.Add(name, email);
+                    }
+
+                    else 
+                    {
+
+                        dictionaryclass[name] = email;
+                    }
+                
+                }
+               
+                if(input[0] == "Sent")
+                {
+                    string name = input[1];
+                    
+        
+                    
+                    if (dictionaryclass.ContainsKey(name))
+                    {
+                       
+                        foreach (var currentName in dictionaryclass)
+                        {
+                            Console.WriteLine($"{currentName.Key} -> {currentName.Value}");
+                        }
+                    }
+               
+                    else 
+
+                    {
+                        Console.WriteLine($"Current {name} doesnt exist");
+                    }
+
+                    
+                
+                }
+            
+
+            }
+
+            foreach (var currentName in dictionaryclass)
+            {
+                Console.WriteLine($"{currentName.Key} -> {currentName.Value}");
+            }
+
+
+
+
+            //dictionaryclass.Add("Ivan", "ivanivan@abv.bg");
+            //dictionaryclass.Add("Petyr", "petyrtudjarov@abv.bg");
+
+            //if (!dictionaryclass.ContainsKey("Ivan"))
+            //{
+            //    dictionaryclass.Add("Ivan", "ivanivan@abv.bg");
+            //}
+
+            //else
+            //{
+            //    dictionaryclass["Ivan"] = "ivanivan@abv.bg";
+
+            //    foreach (var name in dictionaryclass)
+            //    {
+            //        Console.WriteLine($"{name.Key} -> {name.Value}");
+            //    }
+            //}
+        }
+
+    }
+}
---
+++ SuperMarket/SuperMarket/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> productPrice = new Dictionary<string, double>();
+            Dictionary<string, int> productCount = new Dictionary<string, int>();
+
+            while (true)
+            {
+
+                var input = Console.ReadLine().Split();
+                if (input [0] == "stocked")
+                { break; }
+
+                string productName = input[0];
+                double price = double.Parse(input[1]);
+                int count = int.Parse(input[2]);
+                if (!productPrice.ContainsKey(productName))
+                {
+                    productPrice.Add(productName, price);
+                    productCount.Add(productName, count);
+                }
+
+                else
+                {
+                    productPrice[productName] = price;
+                    productCount[productName] += count;
+                }
+            
+            }
+
+            double grandTotal = 0.00;
+            foreach (var product in productPrice)
+            {
+                string productName = product.Key;
+                double price = product.Value;
+                int count = productCount[productName];
+                Console.WriteLine($"{productName} : ${price:F2}*{count }=${price * count:F2}");
+                grandTotal += price * count;
+            }
+
+            Console.WriteLine($"GrandTotal : ${grandTotal :F2}");
+        }
+    }
+}
